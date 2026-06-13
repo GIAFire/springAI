7650ea462d3c6eafa864ba234c4638f4a3a375f2
w <weijbo@163.com>
2026-06-13 23:28:09 +0800

    配置Client

--- src/main/java/com/example/springai/config/ChatClientConfig.java
+++ src/main/java/com/example/springai/config/ChatClientConfig.java
@@ -1,17 +1,43 @@
 package com.example.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
-        // Global role prompt used by regular chat and RAG calls unless overridden per request.
+        // 设置上下文最大记录
+        MessageWindowChatMemory messageBuild = MessageWindowChatMemory.builder().maxMessages(1000).build();
         return builder
-                .defaultSystem("你是一个 Java Spring AI Agent 助手，回答要简洁、准确。")
+                // 重要 默认关键词,可根据业务进行读取配置文件变量定义模板
+                .defaultSystem("你是一个Agent")
+                // 重要 适合固定功能性智能体,在用户提问时,额外补充内容,相当于帮用户补充、优化提问内容
+                .defaultUser(user -> user
+                        .text("{level}")
+                        .param("level", ""))
+                // !!!最重要 请求拦截器,每次请求时从外部获取数据,添加到上下文.如知识库,搜索结果,会话记忆等
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(messageBuild).build())
+                // 设置模型参数
+                .defaultOptions(ChatOptions.builder()
+                        // 小稳定,大热情
+                        .temperature(0.8))
+                // 重要 设置默认工具
+                .defaultTools()
+                // 用于给工具传默认参数
+                .defaultToolContext(Map.of(
+                        "tenantId", "t001",
+                        "system", "mall",
+                        "env", "prod"
+                ))
                 .build();
     }
 }
--- src/main/java/com/example/springai/controller/ChatController.java
+++ src/main/java/com/example/springai/controller/ChatController.java
@@ -1,13 +1,17 @@
 package com.example.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/chat")
@@ -17,9 +21,10 @@ public class ChatController {
     private ChatClient chatClient;
 
     @GetMapping("/chat")
-    public String chat(@RequestParam String msg) {
+    public String chat(@RequestParam String conversationId,@RequestParam String msg) {
         return chatClient.prompt()
                 .user(msg)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .call()
                 .content();
     }
@@ -30,5 +35,32 @@ public class ChatController {
                 .user(msg)
                 .stream()
                 .content();
+    }
+
+    @GetMapping(value = "/chatSseEmitter", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter chatSseEmitter(@RequestParam String conversationId, @RequestParam String msg) {
+        SseEmitter emitter = new SseEmitter(0L);
+
+        chatClient.prompt("")
+                .user(msg)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .stream()
+                .content()
+                .subscribe(
+                        content -> sendMessage(emitter, content),
+                        emitter::completeWithError,
+                        emitter::complete
+                );
+        return emitter;
+    }
+    private void sendMessage(SseEmitter emitter, String content) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("message")
+                    .data(content));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
